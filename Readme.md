2b28d258a7b32a48add7cdc331ec0c80e952973f
Leonardo Pereira <leonardo.caps@gmail.com>
2018-06-09 17:17:19 -0300

    add buttons

--- Readme.md
+++ Readme.md
@@ -7,6 +7,13 @@ Framework Wordpress para a criação de temas com foco em performance
 * E-mail: leonardo@leowpdeveloper.com.br
 * Site: https://leowpdeveloper.com.br
 
+## Tecnologias utilizadas
+* PHP
+* HTML5
+* CSS3
+* SASS ( BEM )
+* Gulp
+
 ## Configuração do projeto
 
 ### Utilização do Gulp
@@ -15,9 +22,21 @@ Framework Wordpress para a criação de temas com foco em performance
 + `gulp watch` para deixar o gulp rodando enquanto o projeto está sendo desenvolvido
 Os arquivos da pasta *dev* são compilados e enviados para a pasta *dist* já minificados
 
-## Tecnologias utilizadas
-* PHP
-* HTML5
-* CSS3
-* SASS ( BEM )
-* Gulp
+## Customizações do framework
+
+### Buttons
+* `leowp-button` classe para o botāo
+* `leowp-button leowp-button__red` classe para o botāo com cor definida.
+* `leowp-button leowp-button__red leowp-button__medium` classe para o botāo com cor e tamanho.
+#### Classes de cores ja definidas
+* `leowp-button leowp-button__red`
+* `leowp-button leowp-button__blue`
+* `leowp-button leowp-button__green`
+* `leowp-button leowp-button__yellow`
+* `leowp-button leowp-button__purple`
+* `leowp-button leowp-button__orange`
+#### Classes de tamanho ja definidas
+* `leowp-button`
+* `leowp-button leowp-button__small`
+* `leowp-button leowp-button__medium`
+* `leowp-button leowp-button__large`
--- header.php
+++ header.php
@@ -3,7 +3,6 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Page Title</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="http://gmpg.org/xfn11" />
     <?php if (is_singular() && pings_open(get_queried_object())) : ?>
@@ -13,9 +12,9 @@
 </head>
 <body <?php body_class(); ?>>
 
-<header class="leowp__header" itemscope itemtype="http://schema.org/WPHeader">
+<header class="leowp-header" itemscope itemtype="http://schema.org/WPHeader">
 
-    <div class="leowp__header--logo">
+    <div class="leowp-header__logo">
         <?php
         /** 
          * verifica se o logo foi adicionado pelo painel do wordpress 
@@ -24,19 +23,19 @@
         if( has_custom_logo() ):
             the_custom_logo();
         else:
-            echo '<a href="'. SITE_HOME . '" class="leowp__header--logo-brand" title="' . SITE_NAME . '">' . SITE_NAME . '</a>';  
+            echo '<a href="'. SITE_HOME . '" class="leowp-header__logo--brand" title="' . SITE_NAME . '">' . SITE_NAME . '</a>';  
         endif;        
         ?>
     </div>
 
-    <nav class="leowp__header--navigation">
+    <nav class="leowp-header__navigation">
        <?php
         if (has_nav_menu('primary')) :
             wp_nav_menu(array(
             'theme_location' => 'primary',
             'menu_id' => 'primary-menu',
-            'menu_class' => 'leowp__header--navigation-menu',
-            'items_wrap' => '<ul class="leowp__header--navigation-menu">%3$s</ul>',
+            'menu_class' => 'leowp-header__navigation--menu',
+            'items_wrap' => '<ul class="leowp-header__navigation--menu">%3$s</ul>',
         ));
         endif;
         ?> 
