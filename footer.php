16730c3ea6ab59bddce5bdf15ff375c317a42ab4
Leonardo Pereira <leonardo.caps@gmail.com>
2018-07-15 13:06:37 -0300

    update footer

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
     <div class="leowp-container__center">
         <p><?php echo sprintf(__('LeoWP Workflow - Framework Wordpress | Desenvolvidor Por: <a href="%s">Leonardo Pereira</a>.', 'leowp-workflow'),'https://leowpdeveloper.com.br'); ?></p>
     </div> 
-</footer
+</footer>
 
 <?php wp_footer(); ?>
 </body>
